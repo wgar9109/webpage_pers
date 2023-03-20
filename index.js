667b1b9d05448326bced1f1a1ce60bca492e90e8
wgar9109 <tylergary09@gmail.com>
2023-03-20 06:50:52 -0500

    Brushing up final touches

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-StyleSheet.toString(PageTransitionEvent)
-console.log(list-items)
+const spinCube = document.querySelector(".section-1");
+
+spinCube.style.position = "absolute";
+spinCube.style.display = "initial";
+spinCube.style.backgroundColor = "black";
+
+console.log(spinCube);
